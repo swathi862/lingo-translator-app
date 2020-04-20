b56e5d957fdcc51842215fda46d3e4298bbf2ff6
Jordan Castelloe <jordan.castelloe@gmail.com>
2020-04-20 11:33:32 -0400

    added dom printer module

---
+++ scripts/domPrinter.js
@@ -0,0 +1,27 @@
+// The purpose of this file is to define functions that will return individual HTML components
+
+// You should not call any of these functions in this file. Instead, you should call them in the individual file for your language (e.g. hindi.js)
+
+
+// For example, we can define a function here that prints an H1 element
+
+const h1 = (text, classNames) => {
+    return `<h1 class="${classNames}">${text}</h1>`
+}
+
+// Go to french.js to see how we can execute this function
+
+
+// -------------------- For reference! -----------------//
+
+// Here are some other ways to right the exact same function we wrote above:
+
+// function h1(text, classNames){
+//     return `<h1 class="${classNames}">${text}</h1>`
+// }
+
+// const h1 = function(text, classNames){
+//     return `<h1 class="${classNames}">${text}</h1>`
+// }
+
+// const h1 = (text, classNames) => `<h1 class="${classNames}">${text}</h1>`
