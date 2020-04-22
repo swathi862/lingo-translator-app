e3c6b04e96d58c2bbfc20194d808e98375455dcc
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-04-22 15:31:19 -0400

    made a universal function for countriesSpoken...holy shite

--- scripts/spanish.js
+++ scripts/spanish.js
@@ -46,4 +46,51 @@ const spanishData = {
       whatsYourName: "cuál es tu nombre?"
     }
   };
+//constant and function to click the spanish button on the navigation bar.
+  const spanishButton = document.querySelector("#spanish")
+  spanishButton.addEventListener("click", function(){
+  // console.log("you clicked the spanish button")
+  document.querySelector("#language-container").innerHTML = h1(spanishData.name, "spanish-heading")
+  document.querySelector("#language-container").innerHTML += printCountriesSpoken(spanishData)
+  })
 
+  // document.querySelector("#language-container").innerHTML = `<div class="container">
+  // <div class="row">
+  // <div class="col-sm">
+  // <h3>Countries Spoken</h3>
+  // <p>${spanishData.countriesSpoken.length}</p>
+  // </div>
+  // </div>
+  // </div>`
+
+// const createCountriesSpokenComponent = (spanishData.countriesSpoken[i])
+// return `<div class="container">
+// <div class="row">
+// <div class="col-sm">
+// <h3>Countries Spoken</h3>
+// <p>${spanishData.countriesSpoken.length}</p>
+// </div>
+// </div>
+// </div>`
+
+//just trying to get countriesSpoken to print to dom
+//   const contriesSpokenContainer =document.querySelector("#language-container")
+
+// for (let i=0; i < spanishData.countriesSpoken.length; i++){
+//   const countriesSpoken = spanishData.countriesSpoken[i]
+//   contriesSpokenContainer.innerHTML += `<li>${spanishData.countriesSpoken[i]}</li>`
+// }
+  
+
+
+
+
+// return `
+/* <div class="container">
+<div class="row">
+<div class="col-sm">
+<h3>Countries Spoken</h3>
+<p>${countriesSpoken.length}</p>
+</div>
+</div>
+</div>` */
